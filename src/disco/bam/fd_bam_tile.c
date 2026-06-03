a956e3052e04745802524e2e206cc9c3900cbce2
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-06-02 20:32:15 -0700

    cleanup openssl

--- src/disco/bam/fd_bam_tile.c
+++ src/disco/bam/fd_bam_tile.c
@@ -6,7 +6,6 @@
 #include "../../discoh/plugin/fd_plugin.h"
 #include "../../flamenco/gossip/fd_gossip_message.h"
 #include "../../waltz/http/fd_url.h"
-#include "../../waltz/openssl/fd_openssl.h"
 #include "../../waltz/openssl/fd_openssl_tile.h"
 #include "../../tango/fseq/fd_fseq.h"
 #include "../../util/pod/fd_pod_format.h"
@@ -14,7 +13,6 @@
 
 #include <errno.h>
 #include <ctype.h> /* isspace */
-#include <dirent.h> /* opendir */
 #include <limits.h> /* LONG_MIN, USHRT_MAX */
 #include <stdio.h> /* snprintf */
 #include <fcntl.h> /* open, F_SETFL, O_NONBLOCK for generated seccomp */
@@ -1155,59 +1153,6 @@ fd_ossl_keylog_callback( SSL const *  ssl,
 }
 
 static void
-fd_bam_tile_load_certs( SSL_CTX * ssl_ctx ) {
-  X509_STORE * ca_certs = X509_STORE_new();
-  if( FD_UNLIKELY( !ca_certs ) ) {
-    FD_LOG_ERR(( "X509_STORE_new failed" ));
-  }
-
-  static char const default_dir[] = "/etc/ssl/certs/";
-  DIR * dir = opendir( default_dir );
-  if( FD_UNLIKELY( !dir ) ) {
-    FD_LOG_ERR(( "opendir(%s) failed (%i-%s)", default_dir, errno, fd_io_strerror( errno ) ));
-  }
-
-  struct dirent * entry;
-  for( ;; ) {
-    errno = 0; /* Track errors from this readdir() call only */
-    entry = readdir( dir );
-    if( FD_UNLIKELY( !entry ) ) break;
-    if( !strcmp( entry->d_name, "." ) || !strcmp( entry->d_name, ".." ) ) continue;
-
-    char cert_path[ PATH_MAX ];
-    char * p = fd_cstr_init( cert_path );
-    p = fd_cstr_append_text( p, default_dir, sizeof(default_dir)-1 );
-    p = fd_cstr_append_cstr_safe( p, entry->d_name, (ulong)(cert_path+sizeof(cert_path)-1) - (ulong)p );
-    fd_cstr_fini( p );
-
-    if( !X509_STORE_load_locations( ca_certs, cert_path, NULL ) ) {
-      /* Not all files in /etc/ssl/certs are valid certs, so ignore errors */
-      continue;
-    }
-  }
-
-  if( FD_UNLIKELY( errno && errno != ENOENT ) ) {
-    FD_LOG_ERR(( "readdir(%s) failed (%i-%s)", default_dir, errno, fd_io_strerror( errno ) ));
-  }
-
-  STACK_OF(X509) * cert_list = X509_STORE_get1_all_certs( ca_certs );
-  FD_LOG_INFO(( "Loaded %d CA certs from %s into OpenSSL", sk_X509_num( cert_list ), default_dir ));
-  if( fd_log_level_logfile() == 0 ) {
-    for( int i=0; i < sk_X509_num( cert_list ); i++ ) {
-      X509 * cert = sk_X509_value( cert_list, i );
-      FD_LOG_DEBUG(( "Loaded CA cert \"%s\"", X509_NAME_oneline( X509_get_subject_name( cert ), NULL, 0 ) ));
-    }
-  }
-  sk_X509_pop_free( cert_list, X509_free );
-
-  SSL_CTX_set_cert_store( ssl_ctx, ca_certs );
-
-  if( FD_UNLIKELY( 0 != closedir( dir ) ) ) {
-    FD_LOG_ERR(( "closedir(%s) failed (%i-%s)", default_dir, errno, fd_io_strerror( errno ) ));
-  }
-}
-
-static void
 fd_bam_tile_init_openssl( fd_bam_tile_t * ctx,
                              void *             alloc_mem,
                              int                tls_cert_verify ) {
@@ -1241,8 +1186,7 @@ fd_bam_tile_init_openssl( fd_bam_tile_t * ctx,
   }
 
   if( tls_cert_verify ) {
-    fd_bam_tile_load_certs( ssl_ctx );
-    SSL_CTX_set_verify( ssl_ctx, SSL_VERIFY_PEER, NULL );
+    fd_ossl_load_certs( ssl_ctx );
   }
 
   if( FD_LIKELY( ctx->keylog_fd >= 0 ) ) {
