bcde11f80ec3b038e97f56dcbe6a9f2b9f3e5fed
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-06-16 12:56:04 -0700

    Remove duplicate keyload vote OOB test

--- src/disco/keyguard/test_keyload.c
+++ src/disco/keyguard/test_keyload.c
@@ -68,48 +68,6 @@ test_protected_pages( void ) {
   for( ulong i=0UL; i<4096UL; i++ ) FD_TEST( allocated[i]==1 );
 }
 
-void
-test_vote_txn_oob( void ) {
-  uchar data[172];
-  memset( data, 0, sizeof(data) );
-
-  data[0] = 2;    /* signer_cnt */
-  data[1] = 1;    /* ro_signed_cnt = signer_cnt - 1 */
-  data[2] = 1;    /* ro_unsigned_cnt */
-  data[3] = 4;    /* acc_cnt (compact_u16, 1 byte) */
-
-  fd_keyguard_authority_t authority;
-  memset( &authority, 0xAA, sizeof(authority) );
-  memcpy( data + 4, authority.identity_pubkey, 32 );
-
-  uchar vote_prog_id[32] = {
-    0x07, 0x61, 0x48, 0x1d, 0x35, 0x74, 0x74, 0xbb,
-    0x7c, 0x4d, 0x76, 0x24, 0xeb, 0xd3, 0xbd, 0xb3,
-    0xd8, 0x35, 0x5e, 0x73, 0xd1, 0x10, 0x43, 0xfc,
-    0x0d, 0xa3, 0x53, 0x80, 0x00, 0x00, 0x00, 0x00
-  };
-  memcpy( data + 100, vote_prog_id, 32 );
-
-  data[164] = 1;  /* instr_cnt = 1 (compact_u16, 1 byte) */
-  data[165] = 3;  /* index of vote program = acc_cnt - 1 */
-  data[166] = 2;  /* compact_u16 = 2, 1 byte */
-
-  data[167] = 0;
-  data[168] = 1;
-
-  data[169] = 0x80;  /* bit 7 set -> need at least 2 bytes */
-  data[170] = 0x80;  /* bit 7 set -> need 3 bytes */
-  data[171] = 0x01;  /* non-zero, upper bits clear -> valid 3-byte cu16 */
-
-  int res = fd_keyguard_payload_authorize( &authority,
-                                           data,
-                                           sizeof(data),
-                                           FD_KEYGUARD_ROLE_TXSEND,
-                                           FD_KEYGUARD_SIGN_TYPE_ED25519 );
-
-  (void)res;
-}
-
 /* tmp_key_file creates a new (unprotected) key file with random bytes.
    tmp_key_file1 is a variant with chosen bytes.
 
@@ -280,7 +238,6 @@ main( int     argc,
   test_protected_pages();
   test_readonly();
   test_madvise();
-  test_vote_txn_oob();
   test_contact_info_client_id_authorization();
   FD_LOG_NOTICE(( "pass" ));
   return 0;
